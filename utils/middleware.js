8ad113ef7c908240ced82a7f4011431d7ef52ecc
sheikdon <sheikdonabdiresac@gmail.com>
2022-10-07 00:56:50 -0500

    forgot override method

--- utils/middleware.js
+++ utils/middleware.js
@@ -4,6 +4,7 @@
 require('dotenv').config() // Load ENV Variables
 const morgan = require('morgan') // import morgan
 const express = require('express') // import express
+const methodOverride = require('method-override')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 
@@ -12,6 +13,7 @@ const MongoStore = require('connect-mongo')
 // Middleware function
 /////////////////////////////////////////////
 const middleware = (app) => {
+    app.use(methodOverride('_method'))
     app.use(morgan('tiny')) // This is for request logging, the "tiny" argument declares what size of morgan log to use.
     app.use(express.urlencoded({ extended: true })) // this parses urlEncoded request bodies(useful for POST and PUT requests)
     app.use(express.static('public')) // serve files from the public folder statically
