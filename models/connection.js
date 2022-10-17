73bf1f202882cce47ebc08f5e6f052a5a2ff8cb6
sheikdon <sheikdonabdiresac@gmail.com>
2022-10-17 18:57:06 -0500

    flyvtl link

--- models/connection.js
+++ models/connection.js
@@ -9,13 +9,14 @@ const mongoose = require("mongoose") // import mongoose
 /////////////////////////////////////////////
 // this is where we will set up our inputs for our database connect function
 const DATABASE_URL = process.env.DATABASE_URL
+const DEPLOYED_URL = process.env.DEPLOYED_URL
 // here is our DB config object
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
 // establish our connection
-mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connect(DEPLOYED_URL, CONFIG)
 
 // tell mongoose what to do with certain events
 // opens, disconnects, errors
